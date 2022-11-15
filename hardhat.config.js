ae923205582e4f33a6a28c0e4b81584e8913493e
Sandeep Suresh <sureshsa@usc.edu>
2022-11-15 01:14:17 -0500

    Updating abi and bytecode.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,20 @@
 /**
 * @type import('hardhat/config').HardhatUserConfig
 */
-require('dotenv').config();
+require('dotenv').config({path:__dirname+'/.env'})
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
-const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY, MAINNET_API_URL } = process.env;
+console.log(process.env.API_URL)
 module.exports = {
    solidity: "0.8.7",
-   defaultNetwork: "goerli",
+   defaultNetwork: "mainnet",
    networks: {
       hardhat: {},
       mainnet: {
-         url: MAINNET_API_URL,
-         accounts: [`0x${PRIVATE_KEY}`]
+         url: API_URL,
+         accounts: [PRIVATE_KEY]
       }
    },
    etherscan: {
