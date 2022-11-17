2f2f68a26eb715098e944bdd0eb744918977fc93
Sandeep Suresh <sureshsa@usc.edu>
2022-11-16 20:42:29 -0500

    Removing gradient

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -11,7 +11,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1}}>
-      <AppBar position="static" sx={{alignItems: "flex-end", backgroundColor: "transparent", outlineColor: "transparent"}}>
+      <AppBar position="static" sx={{alignItems: "flex-end", backgroundColor: "transparent", backgroundImage: "none"}}>
         <Toolbar>
           <Link href="https://opensea.io/collection/synthetic-dreams-v2-1">
             <Button>Opensea</Button>
